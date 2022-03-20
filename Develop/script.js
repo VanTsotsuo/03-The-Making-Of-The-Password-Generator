744440dd03ff11d12f59d8509f4108d748ba6c75
user.name <Hollowtech@gmail.com>
2022-03-20 17:02:51 -0400

    Add comments on how to tackle code creation

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,13 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
   //Button function conformation logged to console
   console.log("Hey! you clicked the button!")
+   // 1.prompt the user for password criteria
+  //   a. password length 0 < 28
+  //   b.Lowercase, uppercase, special characters
+  // 2.Input validation
+  // 3.Genrate password
+  // 4.Display password on screen
+    
   return "Generated password will go here!"
 }
 // Write password to the #password input
